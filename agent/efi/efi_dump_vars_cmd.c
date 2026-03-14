9ade12190aafafcd8d9ba1be01d3b4d11e7cc3e2
nstarke <starke.nicholas@gmail.com>
2026-03-14 14:08:33 -0500

    Consolidating code

--- agent/efi/efi_dump_vars_cmd.c
+++ agent/efi/efi_dump_vars_cmd.c
@@ -2,7 +2,8 @@
 
 #include "embedded_linux_audit_cmd.h"
 
-#include <csv.h>
+#include "util/output_buffer.h"
+
 #include <errno.h>
 #include <getopt.h>
 #include <json.h>
@@ -16,12 +17,6 @@
 
 #include <efivar/efivar.h>
 
-struct output_buffer {
-	char *data;
-	size_t len;
-	size_t cap;
-};
-
 static void usage(const char *prog)
 {
 	fprintf(stderr,
@@ -30,58 +25,6 @@ static void usage(const char *prog)
 		"  When global --output-http is configured, POST to /:mac/upload/efi-vars\n"
 		"  When global --output-tcp is configured, stream formatted records over TCP\n",
 		prog);
-}
-
-static int output_buffer_append_len(struct output_buffer *buf, const char *text, size_t text_len)
-{
-	size_t need;
-	char *tmp;
-	size_t new_cap;
-
-	if (!buf || (!text && text_len != 0))
-		return -1;
-
-	need = buf->len + text_len + 1;
-	if (need > buf->cap) {
-		new_cap = buf->cap ? buf->cap : 1024;
-		while (new_cap < need)
-			new_cap *= 2;
-		tmp = realloc(buf->data, new_cap);
-		if (!tmp)
-			return -1;
-		buf->data = tmp;
-		buf->cap = new_cap;
-	}
-
-	if (text_len > 0)
-		memcpy(buf->data + buf->len, text, text_len);
-	buf->len += text_len;
-	buf->data[buf->len] = '\0';
-	return 0;
-}
-
-static int output_buffer_append(struct output_buffer *buf, const char *text)
-{
-	if (!text)
-		return -1;
-	return output_buffer_append_len(buf, text, strlen(text));
-}
-
-static int csv_write_to_buf(struct output_buffer *buf, const char *text)
-{
-	const char *in = text ? text : "";
-	size_t in_len = strlen(in);
-	size_t field_sz = (in_len * 2U) + 3U;
-	char *field = malloc(field_sz);
-	size_t written;
-	int ret;
-
-	if (!field)
-		return -1;
-	written = csv_write(field, field_sz, in, in_len);
-	ret = output_buffer_append_len(buf, field, written);
-	free(field);
-	return ret;
 }
 
 static char *hex_encode(const uint8_t *data, size_t len)
--- agent/linux/linux_execute_command_cmd.c
+++ agent/linux/linux_execute_command_cmd.c
@@ -2,7 +2,8 @@
 
 #include "embedded_linux_audit_cmd.h"
 
-#include <csv.h>
+#include "util/output_buffer.h"
+
 #include <errno.h>
 #include <getopt.h>
 #include <json.h>
@@ -15,12 +16,6 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-struct output_buffer {
-	char *data;
-	size_t len;
-	size_t cap;
-};
-
 static void usage(const char *prog)
 {
 	fprintf(stderr,
@@ -29,58 +24,6 @@ static void usage(const char *prog)
 		"  Output honors --output-format as txt, csv, or json\n"
 		"  When global --output-http is configured, POST output to /:mac/upload/cmd\n",
 		prog);
-}
-
-static int output_buffer_append_len(struct output_buffer *buf, const char *text, size_t text_len)
-{
-	size_t need;
-	char *tmp;
-	size_t new_cap;
-
-	if (!buf || (!text && text_len != 0))
-		return -1;
-
-	need = buf->len + text_len + 1;
-	if (need > buf->cap) {
-		new_cap = buf->cap ? buf->cap : 1024;
-		while (new_cap < need)
-			new_cap *= 2;
-		tmp = realloc(buf->data, new_cap);
-		if (!tmp)
-			return -1;
-		buf->data = tmp;
-		buf->cap = new_cap;
-	}
-
-	if (text_len)
-		memcpy(buf->data + buf->len, text, text_len);
-	buf->len += text_len;
-	buf->data[buf->len] = '\0';
-	return 0;
-}
-
-static int output_buffer_append(struct output_buffer *buf, const char *text)
-{
-	if (!text)
-		return -1;
-	return output_buffer_append_len(buf, text, strlen(text));
-}
-
-static int csv_write_to_buf(struct output_buffer *buf, const char *text)
-{
-	const char *in = text ? text : "";
-	size_t in_len = strlen(in);
-	size_t field_sz = (in_len * 2U) + 3U;
-	char *field = malloc(field_sz);
-	size_t written;
-	int ret;
-
-	if (!field)
-		return -1;
-	written = csv_write(field, field_sz, in, in_len);
-	ret = output_buffer_append_len(buf, field, written);
-	free(field);
-	return ret;
 }
 
 static int format_command_output(const char *command,
